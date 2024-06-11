1ba1f1264344335b512a66af3336070274aea0d2
Adilson Quifucussa <adilson.quifucussa@tistech.co.ao>
2024-06-11 15:14:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+app = new express();
+
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res){
+    res.render('welcome');
+});
+
+app.listen(8080, function(){
+    console.log('listening on 8080');
+});
+
